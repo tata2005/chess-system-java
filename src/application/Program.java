3abde4a8591bab0e9b06e76a70316c7f1bd6752d
Tarciso Costa <tarcisocosta3467@gmail.com>
2023-12-19 15:01:44 -0300

    Classe position creada

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello world!");
+		Position position = new Position(2, 4);
+		System.out.println(position);
 
 	}
 
